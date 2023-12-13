33d002e01132929f0d636f1405663fcdaae52cf3
Ignacio Borrell <ignacioborrellr@gmail.com>
2023-12-13 22:30:01 +0100

    calculator

--- calculator.js
+++ calculator.js
@@ -1,66 +1,70 @@
 let calculation = ""
 
+      function totalResult(){
+      document.getElementById("calculation").innerHTML = calculation
+      }
+
       document.getElementById("1").onclick = function(){
         calculation += 1
-        console.log(calculation)
+        totalResult()
       }
       document.getElementById("2").onclick = function(){
         calculation += 2
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("3").onclick = function(){
         calculation += 3
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("4").onclick = function(){
         calculation += 4
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("5").onclick = function(){
         calculation += 5
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("6").onclick = function(){
         calculation += 6
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("7").onclick = function(){
         calculation += 7
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("8").onclick = function(){
         calculation += 8
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("9").onclick = function(){
         calculation += 9
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById(".").onclick = function(){
         calculation += "."
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("=").onclick = function(){
       calculation = eval(calculation)
-      console.log(calculation)
+       totalResult()
       }
       document.getElementById("*").onclick = function(){
         calculation += "*"
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("/").onclick = function(){
         calculation += "/"
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("-").onclick = function(){
         calculation += "-"
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("+").onclick = function(){
         calculation += "+"
-        console.log(calculation)
+         totalResult()
       }
       document.getElementById("Clear").onclick = function(){
         calculation = ""
-        console.log(calculation)
+         totalResult()
       }
